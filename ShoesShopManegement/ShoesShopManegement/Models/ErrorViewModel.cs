52c77bcebe4c395092ae1767951681603ab7a802
PhamDuong39 <phamanhduong30092003@gmail.com>
2023-05-12 16:34:35 +0700

    Initial Project

---
+++ ShoesShopManegement/ShoesShopManegement/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ShoesShopManegement.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
