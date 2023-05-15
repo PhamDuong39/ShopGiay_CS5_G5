b09c0a6fd26af8ddb84a2010264caa1406e1bcfb
PhamDuong39 <phamanhduong30092003@gmail.com>
2023-05-15 12:24:33 +0700

    Initial Model class

---
+++ Data/Models/Bills.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Models
+{
+    public class Bills
+    {
+        public Guid Id { get; set; }
+        public Guid IdUser { get; set; }
+        public DateTime CreateDate { get; set; }
+        public string Note { get; set; }
+        public int Status { get; set; }
+        public Guid IdVoucher { get; set; }
+
+        public List<BillDetails> BillDetails { get; set; }
+        public List<Coupons> Coupons { get; set; }
+        public virtual Users Users { get; set; }
+
+
+    }
+}
---
+++ Data/Models/Coupons.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Models
+{
+    public class Coupons
+    {
+        public Guid Id { get; set; }
+        public int DiscountValue { get; set; }
+        public int Quantity { get; set; }
+        public string VoucherName { get; set; }
+        public virtual Bills Bills { get; set; }
+    }
+}
