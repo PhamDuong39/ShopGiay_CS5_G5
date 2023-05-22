65d6cdff43d3096061f1017dcc25fca5bdc6eda2
ngoctm ngoctm <ngoc0378181600@gmail.com>
2023-05-22 19:18:51 +0700

    user and role

--- Data/Migrations/20230522120202_abc.cs
+++ Data/Migrations/20230522120202_abc.cs
@@ -5,7 +5,7 @@ using Microsoft.EntityFrameworkCore.Migrations;
 
 namespace Data.Migrations
 {
-    public partial class test : Migration
+    public partial class abc : Migration
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
