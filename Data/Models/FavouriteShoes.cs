dad73c4df96d69c4f8734f8051c67499198f2122
Doanh Thái Bùi <88380128+Dthai16gg@users.noreply.github.com>
2023-05-23 01:41:06 +0700

    fix favouriteshoes

--- Data/Models/FavouriteShoes.cs
+++ Data/Models/FavouriteShoes.cs
@@ -1,18 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace Data.Models;
 
-namespace Data.Models
+public class FavouriteShoes
 {
-    public class FavouriteShoes
-    {
-        public Guid Id { get; set; }
-        public Guid IdShoeDetail { get; set; }
-        public int Status { get; set; }
-        public Guid IdUser { get; set; }
-        public virtual ShoeDetails ShoeDetails { get; set; }
-        public virtual Users Users { get; set; }
-    }
+    public Guid Id { get; set; }
+
+    public Guid IdShoeDetail { get; set; }
+
+    public Guid IdUser { get; set; }
+
+    public virtual ShoeDetails ShoeDetails { get; set; }
+
+    public int Status { get; set; }
+
+    public virtual Users Users { get; set; }
 }
