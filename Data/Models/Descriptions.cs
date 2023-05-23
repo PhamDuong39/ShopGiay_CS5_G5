4a084abc10e0e3b5276596ab09d8ffa957a812e1
Doanh Thái Bùi <88380128+Dthai16gg@users.noreply.github.com>
2023-05-23 15:36:08 +0700

    update file

--- Data/Models/Descriptions.cs
+++ Data/Models/Descriptions.cs
@@ -1,18 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace Data.Models;
 
-namespace Data.Models
+public class Descriptions
 {
-    public class Descriptions
-    {
-        public Guid Id { get; set; }
-        public Guid IdShoeDetail { get; set; }
-        public string Note1 { get; set; }
-        public string Note2 { get; set; }
-        public string Note3 { get; set; }
-        public virtual ShoeDetails ShoeDetails { get; set; }
-    }
+    public Guid Id { get; set; }
+
+    public Guid IdShoeDetail { get; set; }
+
+    public string Note1 { get; set; }
+
+    public string Note2 { get; set; }
+
+    public string Note3 { get; set; }
+
+    public virtual ShoeDetails ShoeDetails { get; set; }
 }
