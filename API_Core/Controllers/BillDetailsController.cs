48c6d0087009fc4d7495108eafe98962be460ea3
Doanh Thái Bùi <88380128+Dthai16gg@users.noreply.github.com>
2023-05-26 15:53:05 +0700

    update billdetails, update colorcontroller

--- API_Core/Controllers/BillDetailsController.cs
+++ API_Core/Controllers/BillDetailsController.cs
@@ -9,92 +9,133 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace API_Core.Controllers
 {
-  [Route("api/[controller]")]
-  [ApiController]
-  public class BillDetailsController : ControllerBase
-  {
-    public IAllRepositories<BillDetails> _irepos;
-    public IAllRepositories<ShoeDetails> _ishoesrepos;
+    using Microsoft.AspNetCore.Mvc.ModelBinding;
 
-    AppDbContext DbContext;
-
-    public BillDetailsController()
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BillDetailsController : ControllerBase
     {
-      DbContext = new AppDbContext();
-      AllRepositories1<BillDetails> repos = new AllRepositories1<BillDetails>(DbContext, DbContext.BillDetails);
-      AllRepositories1<ShoeDetails> reposShoes = new AllRepositories1<ShoeDetails>(DbContext, DbContext.ShoeDetails);
+        public IAllRepositories<BillDetails> _irepos;
+        public IAllRepositories<ShoeDetails> _ishoesrepos;
 
-      _irepos = repos;
-      _ishoesrepos = reposShoes;
+        AppDbContext DbContext;
 
-    }
-
-    // GET: api/<BillDetailsController>
-    [HttpGet]
-    public IEnumerable<BillDetails> GetAllBillDetails()
-    {
-      return _irepos.GetAll();
-    }
-
-    // GET api/<BillDetailsController>/5
-    [HttpGet("{id}")]
-    public BillDetails Getone(Guid id)
-    {
-      return _irepos.GetAll().FirstOrDefault(p => p.Id == id);
-    }
-
-    //POST api/<BillDetailsController>
-    [HttpPost]
-    public string Post(Guid IdShoeDetail, Guid IdBill, int price, int quantity) // tra ve string ne
-    {
-      if (!_ishoesrepos.GetAll().Any(p => p.Id == IdShoeDetail))
-      {
-        return "Loại giày không tồn tại";
-      }
-      else
-      {
-        //kiem tra neu hoa don da ton tai va sp da ton tai thi tang quantity
-        if (_irepos.GetAll().Any(p => p.IdBill == IdBill && p.IdShoeDetail == IdShoeDetail))
+        public BillDetailsController()
         {
-          BillDetails bds = _irepos.GetAll().FirstOrDefault(p => p.IdBill == IdBill && p.IdShoeDetail == IdShoeDetail);
-          bds.Quantity += quantity;
-          _irepos.Update(bds);
-          return "Thêm thành công";
-        }
-        BillDetails bd = new BillDetails();
-        bd.IdShoeDetail = IdShoeDetail;
-        bd.IdBill = IdBill;
-        bd.Price = price;
-        bd.Quantity = quantity;
-        if (_irepos.Create(bd))
-        {
-          return "Thêm thành công";
-        }
-        else
-        {
-          return "Them That bai";
-        }
-      }
-    }
+            DbContext = new AppDbContext();
+            AllRepositories1<BillDetails> repos = new AllRepositories1<BillDetails>(DbContext, DbContext.BillDetails);
+            AllRepositories1<ShoeDetails> reposShoes = new AllRepositories1<ShoeDetails>(DbContext, DbContext.ShoeDetails);
 
-    // PUT api/<BillDetailsController>/5
-    [HttpPut("{id}")]
-    public bool UpdateBilldetails(Guid id, Guid IdShoeDetail, Guid IdBill, int price, int quantity)
-    {
-      var obj = _irepos.GetAll().FirstOrDefault(p => p.Id == id);
-      obj.IdShoeDetail = IdShoeDetail;
-      obj.IdBill = IdBill;
-      obj.Price = price;
-      obj.Quantity = quantity;
-      return _irepos.Update(obj);
-    }
+            _irepos = repos;
+            _ishoesrepos = reposShoes;
 
-    // DELETE api/<BillDetailsController>/5
-    [HttpDelete("{id}")]
-    public bool Delete(Guid id)
-    {
-      var obj = _irepos.GetAll().FirstOrDefault(p => p.Id == id);
-      return _irepos.Delete(obj);
+        }
+
+        // GET: api/<BillDetailsController>
+        [HttpGet]
+        public IEnumerable<BillDetails> GetAllBillDetails()
+        {
+            return _irepos.GetAll();
+        }
+
+        // GET api/<BillDetailsController>/5
+        [HttpGet("{id}")]
+        public BillDetails Getone(Guid id)
+        {
+            return _irepos.GetAll().FirstOrDefault(p => p.Id == id);
+        }
+
+        //POST api/<BillDetailsController>
+        [HttpPost]
+        public string Post(Guid IdShoeDetail, Guid IdBill, int price, int quantity) // tra ve string ne
+        {
+            if (_ishoesrepos.GetAll().FirstOrDefault(p => p.Id == IdShoeDetail).AvailableQuantity < quantity)
+            {
+                return "Số lượng không đủ";
+            }
+            if (!_ishoesrepos.GetAll().Any(p => p.Id == IdShoeDetail))
+            {
+                return "Loại giày không tồn tại";
+            }
+            else
+            {
+                //kiem tra neu hoa don da ton tai va sp da ton tai thi tang quantity
+                if (_irepos.GetAll().Any(p => p.IdBill == IdBill && p.IdShoeDetail == IdShoeDetail))
+                {
+                    BillDetails bds = _irepos.GetAll().FirstOrDefault(p => p.IdBill == IdBill && p.IdShoeDetail == IdShoeDetail);
+                    if (quantity > _ishoesrepos.GetAll().FirstOrDefault(p => p.Id == IdShoeDetail).AvailableQuantity)
+                    {
+                        return "Số lượng không đủ";
+                    }
+                    bds.Quantity += quantity;
+                    //update lai so luong trong kho
+                    ShoeDetails sd = _ishoesrepos.GetAll().FirstOrDefault(p => p.Id == IdShoeDetail);
+                    sd.AvailableQuantity -= quantity;
+                    _ishoesrepos.Update(sd);
+                    _irepos.Update(bds);
+                    return "Thêm thành công";
+                }
+                BillDetails bd = new BillDetails();
+                bd.IdShoeDetail = IdShoeDetail;
+                bd.IdBill = IdBill;
+                bd.Price = price;
+                bd.Quantity = quantity;
+                if (_irepos.Create(bd))
+                {
+                    return "Thêm thành công";
+                }
+                else
+                {
+                    return "Them That bai";
+                }
+            }
+        }
+
+        // PUT api/<BillDetailsController>/5
+        [HttpPut("{id}")]
+        public string UpdateBilldetails(Guid id, Guid IdShoeDetail, Guid IdBill, int price, int quantity)
+        {
+            //check sp co ton tai khong
+            if (!_ishoesrepos.GetAll().Any(p => p.Id == IdShoeDetail))
+            {
+                return "Loại giày không tồn tại";
+            }
+            //check so luong co du khong
+            else if (_ishoesrepos.GetAll().FirstOrDefault(p => p.Id == IdShoeDetail).AvailableQuantity < quantity)
+            {
+                return "Số lượng không đủ";
+            }
+            else
+            {
+                var obj = _irepos.GetAll().FirstOrDefault(p => p.Id == id);
+                obj.IdShoeDetail = IdShoeDetail;
+                obj.IdBill = IdBill;
+                obj.Price = price;
+                obj.Quantity = quantity;
+
+                ShoeDetails sd = _ishoesrepos.GetAll().FirstOrDefault(p => p.Id == IdShoeDetail);
+                //nếu số lượng update lớn hơn số lượng ban đ    ầu thì update lại kho
+                if (obj.Quantity > quantity)
+                    sd.AvailableQuantity -= (obj.Quantity - quantity);
+                else
+                    sd.AvailableQuantity += sd.AvailableQuantity + (obj.Quantity - quantity); //
+                _ishoesrepos.Update(sd);
+                _irepos.Update(obj);
+                //khi update lại số lượng nếu số lương
+                return "Sửa thành công";
+            }
+        }
+
+        // DELETE api/<BillDetailsController>/5
+        [HttpDelete("{id}")]
+        public bool Delete(Guid id)
+        {
+            var obj = _irepos.GetAll().FirstOrDefault(p => p.Id == id);
+            //neu xoa billdetail thi update lai so luong trong kho
+            ShoeDetails sd = _ishoesrepos.GetAll().FirstOrDefault(p => p.Id == obj.IdShoeDetail);
+            sd.AvailableQuantity += obj.Quantity;
+            _ishoesrepos.Update(sd);
+            return _irepos.Delete(obj);
+        }
     }
-  }
 }
