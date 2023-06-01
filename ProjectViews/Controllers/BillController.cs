426733ad93fbb032a91d71ec8ab02497734b38d9
PhamDuong39 <phamanhduong30092003@gmail.com>
2023-06-02 01:36:05 +0700

    Complete task payment

--- ProjectViews/Controllers/BillController.cs
+++ ProjectViews/Controllers/BillController.cs
@@ -25,6 +25,14 @@ namespace ProjectViews.Controllers
             string apiData = await response.Content.ReadAsStringAsync();
             var bills = JsonConvert.DeserializeObject<List<Bills>>(apiData);
 
+            string apiURLuser = $"https://localhost:7109/api/User/get-all-user";
+            var responseGetuser = await _httpClient.GetAsync(apiURLuser);
+            string apiDatauser = await responseGetuser.Content.ReadAsStringAsync();
+            var user = JsonConvert.DeserializeObject<List<Users>>(apiDatauser);
+
+            ViewData["lstUser"] = new SelectList(user, "Id", "Username");
+
+
             return View(bills);
         }
 
@@ -44,10 +52,56 @@ namespace ProjectViews.Controllers
             var bill = JsonConvert.DeserializeObject<Bills>(apiDataBill);
             var billDTs = JsonConvert.DeserializeObject<List<BillDetails>>(apiDataBillDT);
 
+            // Get coupon
             var apiURLCoupon = $"https://localhost:7109/api/Coupons/{bill.IdCoupon}";
             var responseGetCoupon = await _httpClient.GetAsync(apiURLCoupon);
             string apiDataCoupon = await responseGetCoupon.Content.ReadAsStringAsync();
             var coupon = JsonConvert.DeserializeObject<Coupons>(apiDataCoupon);
+
+            // show username by id
+            string apiURLuser = $"https://localhost:7109/api/User/get-all-user";
+            var responseGetuser = await _httpClient.GetAsync(apiURLuser);
+            string apiDatauser = await responseGetuser.Content.ReadAsStringAsync();
+            var user = JsonConvert.DeserializeObject<List<Users>>(apiDatauser);
+            ViewData["lstUser"] = new SelectList(user, "Id", "Username");
+            
+            // location
+            string apiURLLocation = $"https://localhost:7109/api/Location";
+            var responseGetLocation = await _httpClient.GetAsync(apiURLLocation);
+            string apiDataLocation = await responseGetLocation.Content.ReadAsStringAsync();
+            var location = JsonConvert.DeserializeObject<List<Location>>(apiDataLocation);
+
+            List<SelectListItem> selectListLocation = new List<SelectListItem>();
+            foreach (var item in location)
+            {
+                selectListLocation.Add(new SelectListItem
+                {
+                    Value = item.Id.ToString(),
+                    Text = item.Stage + "" + item.District + "" + item.Ward + "" + item.Street + "" + item.Address
+                });
+            }   
+            ViewData["lstLocation"] = selectListLocation;
+
+            // Payment method
+            string apiURLPayment = $"https://localhost:7109/api/PaymentMethod";
+            var responseGetPayment = await _httpClient.GetAsync(apiURLPayment);
+            string apiDataPayment = await responseGetPayment.Content.ReadAsStringAsync();
+            var paymentMethod = JsonConvert.DeserializeObject<List<PaymentMethods>>(apiDataPayment);
+            ViewData["lstPaymentmethod"] = new SelectList(paymentMethod, "Id", "NameMethod");
+
+            // ship method
+            string apiURLShip = $"https://localhost:7109/api/ShipMethod";
+            var responseGetShip = await _httpClient.GetAsync(apiURLShip);
+            string apiDataShip = await responseGetShip.Content.ReadAsStringAsync();
+            var shipMethod = JsonConvert.DeserializeObject<List<ShipAdressMethod>>(apiDataShip);
+            ViewData["lstShipmethod"] = new SelectList(shipMethod, "Id", "NameAddress");
+
+            // Shoe detail
+            string apiURLShoes = $"https://localhost:7109/api/ShoeDetails/get-all-shoeDetails";
+            var responseGetShoes = await _httpClient.GetAsync(apiURLShoes);
+            string apiDataShoees = await responseGetShoes.Content.ReadAsStringAsync();
+            var shoes = JsonConvert.DeserializeObject<List<ShoeDetails>>(apiDataShoees);
+            ViewData["lstShoes"] = new SelectList(shoes, "Id", "Name");
 
             BillsViewModel billViewMD = new BillsViewModel();
             int price = 0;
@@ -58,8 +112,10 @@ namespace ProjectViews.Controllers
 
             billViewMD.bill = bill;
             billViewMD.lstBillDT = billDTs;
-            billViewMD.sumPrice = price - price * (coupon.DiscountValue / 100);
-
+            billViewMD.DiscountMoney = (double)price * (Convert.ToDouble(coupon.DiscountValue) * 0.01);     
+            billViewMD.deliveryFee = shipMethod.FirstOrDefault(p => p.Id == bill.IdShipAdressMethod).Price;
+            billViewMD.sumPrice = ((double)price + shipMethod.FirstOrDefault(p => p.Id == bill.IdShipAdressMethod).Price) - (double)price * (Convert.ToDouble(coupon.DiscountValue) * 0.01);
+            billViewMD.NoDiscountPrice = ((double)price + shipMethod.FirstOrDefault(p => p.Id == bill.IdShipAdressMethod).Price);
             return View(billViewMD);
         }
 
@@ -115,7 +171,7 @@ namespace ProjectViews.Controllers
         {
             string apiURL = $"https://localhost:7109/api/Bill/UpdateBill?id={Id}&IdUser={bill.IdUser}&Note={bill.Note}&status={bill.Status}&IdCoupon={bill.IdCoupon}&IdShipMethod={bill.IdShipAdressMethod}&IdLocation={bill.IdLocation}&IdPaymentMethod={bill.IdPaymentMethod}";
             var content = new StringContent(JsonConvert.SerializeObject(bill), Encoding.UTF8, "application/json");
-            var response = await _httpClient.PostAsync(apiURL , content);
+            var response = await _httpClient.PutAsync(apiURL , content);
 
             if (response.IsSuccessStatusCode)
             {
