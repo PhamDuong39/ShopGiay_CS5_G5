20f8ae9e433f8c25eaa1ae142ec97fc86e16dc7c
Dthai16gg <buithai16102003@gmail.com>
2023-06-09 12:34:56 +0700

    add controller

---
+++ ProjectViews/Areas/User/Controllers/CategoryController.cs
@@ -0,0 +1,23 @@
+using Data.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+namespace ProjectViews.Areas.User.Controllers
+{
+  [Area("User")]
+  public class CategoryController : Controller
+  {
+    private readonly HttpClient _httpClient;
+    public CategoryController()
+    {
+      _httpClient = new HttpClient();
+    }
+    public async Task<IActionResult> ShowProduct()
+    {
+      string apiShoes = $"https://localhost:7109/api/ShoeDetails/get-all-shoeDetails";
+      var responseShoes = await _httpClient.GetAsync(apiShoes);
+      string apiDataShoes = await responseShoes.Content.ReadAsStringAsync();
+      var shoeDetails = JsonConvert.DeserializeObject<List<ShoeDetails>>(apiDataShoes);
+      return View(shoeDetails);
+    }
+  }
+}
