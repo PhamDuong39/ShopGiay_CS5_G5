34852ccffbbe87f83c74bf811e841e60dfaa8ea0
Dthai16gg <buithai16102003@gmail.com>
2023-06-11 17:48:43 +0700

    fix controller

--- ProjectViews/Controllers/CartDetailController.cs
+++ ProjectViews/Controllers/CartDetailController.cs
@@ -1,6 +1,4 @@
-﻿using Data.IRepositories;
-using Data.Models;
-using Data.Repositories;
+﻿using Data.Models;
 using Data.ShopContext;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
@@ -24,61 +22,6 @@ namespace ProjectViews.Controllers
     [HttpGet]
     public async Task<IActionResult> Show()
     {
-
-        private readonly HttpClient _httpClient;
-        private readonly IAllRepositories<CartDetails> _cartRepository;
-        private readonly IAllRepositories<ShoeDetails> _shoeRepository;
-        private readonly AppDbContext _dbContext;
-        public CartDetailController()
-        {
-            _dbContext = new AppDbContext();
-            _cartRepository = new AllRepositories1<CartDetails>(_dbContext, _dbContext.CartDetails);
-            _shoeRepository = new AllRepositories1<ShoeDetails>(_dbContext, _dbContext.ShoeDetails);
-            _httpClient = new HttpClient();
-        }
-        [HttpGet]
-        public async Task<IActionResult> Show()
-        {
-            
-            string apiURL = $"https://localhost:7109/api/CartDetail";
-            string apiUrlShoe = $"https://localhost:7109/api/ShoeDetails/get-all-shoeDetails";
-            string apiImage = $"https://localhost:7109/api/Images/get-all-image";
-            string apiUser = $"https://localhost:7109/api/Carts";
-
-            var response = await _httpClient.GetAsync(apiURL);
-            var reponce2 = await _httpClient.GetAsync(apiUrlShoe);
-            var reponce3 = await _httpClient.GetAsync(apiImage);
-            var reponce4 = await _httpClient.GetAsync(apiUser);
-
-            var apiData = await response.Content.ReadAsStringAsync();
-            var apiDataShoe = await reponce2.Content.ReadAsStringAsync();
-            var apiDataImage = await reponce3.Content.ReadAsStringAsync();
-            var apiDataUser = await reponce4.Content.ReadAsStringAsync();
-
-            var cartdetail = JsonConvert.DeserializeObject<List<CartDetails>>(apiData);
-            var shoes = JsonConvert.DeserializeObject<List<ShoeDetails>>(apiDataShoe);
-            var image = JsonConvert.DeserializeObject<List<Images>>(apiDataImage);
-            var user = JsonConvert.DeserializeObject<List<Carts>>(apiDataUser);
-
-            ViewData["Name"] = new SelectList(shoes, "Id", "Name");
-            ViewData["SellPrice"] = new SelectList(shoes, "Id", "SellPrice");
-            List<CartDetailModel> lstModel = new List<CartDetailModel>();
-            //int sum = 0;
-            foreach (var item in cartdetail)
-            {
-                CartDetailModel model = new CartDetailModel();
-                model.cartDetail = item;
-                model.Id = item.Id;
-                model.quantity = item.Quantity;
-                
-                var shoe = shoes.FirstOrDefault(s => s.Id == item.IdShoeDetail);
-                model.name = shoe.Name;
-                model.price = shoe.SellPrice;
-                model.ToTalPrice = item.Quantity * shoe.SellPrice;
-                var imageShoe = image.FirstOrDefault(p => p.IdShoeDetail == item.IdShoeDetail);
-                model.ImageSource = imageShoe.ImageSource;
-                lstModel.Add(model);
-            }
 
       string apiURL = $"https://localhost:7109/api/CartDetail";
       string apiUrlShoe = $"https://localhost:7109/api/ShoeDetails/get-all-shoeDetails";
@@ -117,66 +60,6 @@ namespace ProjectViews.Controllers
         lstModel.Add(model);
       }
 
-
-            var content = new StringContent(JsonConvert.SerializeObject(cartDetails), Encoding.UTF8, "application/json");
-            var response = await _httpClient.PutAsync(apiURL, content);
-            if (response.IsSuccessStatusCode)
-            {
-                return this.RedirectToAction("Show");
-            }
-            return this.View();
-        }
-        [HttpGet]
-        public async Task<IActionResult> Delete2(Guid Id)
-        {
-            string apiURL = $"https://localhost:7109/api/CartDetail/{Id}";
-            var response = await _httpClient.DeleteAsync(apiURL);
-            if (response.IsSuccessStatusCode)
-            {
-                return this.RedirectToAction("Show");
-            }
-            return this.RedirectToAction("Show");
-        }
-        //Ở đây này :) cứu
-        //[HttpPost]
-        //public async Task<IActionResult> Pay(Guid iduser)
-        //{
-        //    string apiUrl = $"https://localhost:7109/api/CartDetail/payment";
-        //    var content = System.Text.Json.JsonSerializer.Serialize(iduser);
-        //    var bodyContent = new StringContent(content, Encoding.UTF8, "application/json");
-        //    // Call API
-        //    var postResult = await _httpClient.PostAsync(apiUrl, bodyContent);
-        //    return this.RedirectToAction("Show");
-        //}
-        [HttpPost]
-        public IActionResult Pay(Guid idUser)
-        {
-            var cartDetails = _cartRepository.GetAll().Where(p => p.IdUser == idUser).ToList();
-
-            if (cartDetails.Count == 0)
-            {
-                TempData["Message"] = "Giỏ hàng không tồn tại";
-                return RedirectToAction("Show");
-            }
-
-            foreach (var cartDetail in cartDetails)
-            {
-                var shoeDetail = _shoeRepository.GetAll().FirstOrDefault(p => p.Id == cartDetail.IdShoeDetail);
-                if (shoeDetail != null)
-                {
-                    shoeDetail.AvailableQuantity -= cartDetail.Quantity;
-                    _shoeRepository.Update(shoeDetail);
-                }
-            }
-
-            foreach (var cartDetail in cartDetails)
-            {
-                _cartRepository.Delete(cartDetail);
-            }
-
-            TempData["Message"] = "Thanh toán thành công";
-            return RedirectToAction("Show");
-        }
       return View(lstModel);
     }
     //public async Task<IActionResult> Details(Guid Id)
